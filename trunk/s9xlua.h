6356610dd144eaf6a1da2ec37a302b1d060cc244
gochaism <gochaism@791f8146-ad31-11dd-a35d-6bd55b4a219c>
2008-11-08 02:22:18 +0000

    add missing files as well (perhaps)

---
+++ trunk/s9xlua.h
@@ -0,0 +1,27 @@
+#ifndef _S9XLUA_H
+#define _S9XLUA_H
+
+
+// Just forward function declarations 
+
+void S9xLuaWrite(uint32 addr);
+void S9xLuaFrameBoundary();
+int S9xLoadLuaCode(const char *filename);
+void S9xLuaStop();
+int S9xLuaRunning();
+
+int S9xLuaUsingJoypad(int);
+int S9xLuaReadJoypad(int);
+int S9xLuaSpeed();
+bool8 S9xLuaRerecordCountSkip();
+
+void S9xLuaGui(uint16 *, int ppl, int width, int height);
+
+// And some interesting REVERSE declarations!
+char *S9xGetFreezeFilename(int slot);
+
+// See getset.h!
+//inline void S9xLuaWriteInform(uint32 address);
+
+
+#endif
